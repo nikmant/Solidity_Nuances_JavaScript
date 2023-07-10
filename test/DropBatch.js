8a820809d6e7ef3d4bcbfbac5c9761cebbe59957
Cola <colam@yandex.ru>
2023-07-10 11:58:40 +0300

    7

--- test/DropBatch.js
+++ test/DropBatch.js
@@ -1,54 +1,59 @@
-
 const { expect } = require("chai");
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
-describe("Token contract", function () {
+describe("Testing DropBatch", function () {
     async function deployTokenFixture() {
-      const Token = await ethers.getContractFactory("TokenERC20");
+
       const [owner, addr1, addr2] = await ethers.getSigners();
+
+      const Token = await ethers.getContractFactory("TokenERC20");
       const hardhatToken = await Token.deploy('AAA', 'AAA', 1_000_000_000);  
       await hardhatToken.deployed();
-      console.log("    Token address: ",hardhatToken.address);
-      return { hardhatToken, owner, addr1, addr2 };
+      console.log("    TokenERC20 address: ", hardhatToken.address);
+
+      const DropBatch = await ethers.getContractFactory("DropBatch");
+      const hardhatDropBatch = await DropBatch.deploy();  
+      await hardhatDropBatch.deployed();
+      console.log("    DropBatch address: ", hardhatDropBatch.address);
+
+      return { hardhatToken, hardhatDropBatch, owner, addr1, addr2 };
     }
 
   describe("Deployment", function () {
-    it("Should assign the total supply of tokens to the owner", async function () {
-      const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
-      const ownerBalance = await hardhatToken.balanceOf(owner.address);
-      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    it("Address != 0", async function () {
+      const { hardhatDropBatch } = await loadFixture(deployTokenFixture);
+      expect(hardhatDropBatch.address).to.not.equal(0);
     });
   });
 
-  describe("Transactions", function () {
-    it("Should transfer tokens between accounts", async function () {
-      const { hardhatToken, owner, addr1, addr2 } = await loadFixture(deployTokenFixture);
-      await expect(hardhatToken.transfer(addr1.address, 50))
-        .to.changeTokenBalances(hardhatToken, [owner, addr1], [-50, 50]);
-      await expect(hardhatToken.connect(addr1).transfer(addr2.address, 50))
-        .to.changeTokenBalances(hardhatToken, [addr1, addr2], [-50, 50]);
-    });
-
-    it("should emit Transfer events", async function () {
-      const { hardhatToken, owner, addr1, addr2 } = await loadFixture(deployTokenFixture);
-      await expect(hardhatToken.transfer(addr1.address, 50))
-        .to.emit(hardhatToken, "Transfer").withArgs(owner.address, addr1.address, 50)
-      await expect(hardhatToken.connect(addr1).transfer(addr2.address, 50))
-        .to.emit(hardhatToken, "Transfer").withArgs(addr1.address, addr2.address, 50)
-    });
-
-    it("Should fail if sender doesn't have enough tokens", async function () {
-      const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
-      const initialOwnerBalance = await hardhatToken.balanceOf(
-        owner.address
-      );
-      await expect(
-        hardhatToken.connect(addr1).transfer(owner.address, 1)
-      ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
-
-      expect(await hardhatToken.balanceOf(owner.address)).to.equal(
-        initialOwnerBalance
-      );
+  describe("Work with DropBatch", function () {
+    it("Add token", async function () {
+      const { hardhatToken, hardhatDropBatch, owner } = await loadFixture(deployTokenFixture);
+      hardhatDropBatch.addTokenToDrop('AAA', 1_000_000, 10);
     });
   });
+
+  describe("Work with DropBatch", function () {
+    it("Add token", async function () {
+      const { hardhatToken, hardhatDropBatch, owner, addr1 } = await loadFixture(deployTokenFixture);
+      hardhatDropBatch.TestApprove(hardhatToken.address, addr1.address, 12357);
+      console.log( await hardhatToken.allowance(owner.address, addr1.address) );
+      
+      // approve
+      await hardhatToken.connect(owner).approve(addr1.address, 12357);
+      await hardhatToken.connect(owner).approve(hardhatDropBatch.address, 12357);
+      console.log("allowance from ",owner.address,"to",addr1.address,"=", await hardhatToken.allowance(owner.address, addr1.address) );
+
+      await hardhatToken.connect(addr1).transferFrom(owner.address, addr1.address, 123);
+
+      console.log( await hardhatToken.balanceOf(owner.address) );
+      console.log( await hardhatToken.balanceOf(addr1.address) );
+      console.log( await hardhatToken.balanceOf(hardhatToken.address) );
+
+      // console.log( await hardhatToken.allowance(owner.address, 0) );
+      // expect(hardhatDropBatch.address).to.not.equal(0);
+    });
+  });
+  
+
 });
